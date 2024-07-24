cfac0d62a1574421638483a6f535deb7a7f8df9f
Robbie <robbie331@>
2024-07-24 13:54:23 +0900

    1-2 scanner 사칙연산 기호 전달

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,20 +1,18 @@
 package calculator;
 
 import java.util.Scanner;
+//사칙연산 기호는 String도 가능하지만 char를 이용하여 sc.next().charAt(0); 사용하여 구현
+//charAt(0) 사칙연산기호로 변경해줌 0= 첫번째
+//next()는 striong 에 사용되지만 .charAt()을 통해 char 로 반환가능
 
 public class App {
             public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
 
-                System.out.print("첫 번째 숫자를 입력하세요: ");
-                int num1 = sc.nextInt();
-                // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-                System.out.print("두 번째 숫자를 입력하세요: ");
-                // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-                int num2 = sc.nextInt();
+                System.out.println("사칙연산 기호를 입력하세요: ");
+                char operator = sc.next().charAt(0);
 
-
-                System.out.println("num1, num2 = "+num1+","+num2);
+                System.out.println("operator = " + operator);
 
             }
         }
