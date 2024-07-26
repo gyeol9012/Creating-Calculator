3af637d7a250fd4599a52b124bb67ade3b8e1f7a
Robbie <robbie331@>
2024-07-26 15:52:00 +0900

    1-5 연산결과 10개를 배열에 저장

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 //
 public class App {
     public static void main(String[] args) {
+        Integer[] Array =new Integer[10]; // 연산결과를 10개 저장할 수 있는 배열 생성
+        int count = 0; //연산 결과가 저장된 배열의 마지막 index를 저장하는 변수 선언
         Scanner sc = new Scanner(System.in);
          while(true){
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -13,7 +15,6 @@ public class App {
             int num2 = sc.nextInt();
             System.out.println("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0);
-            System.out.println("operator = " + operator);
             int result = 0;
             switch (operator) {
                 case '+':
@@ -37,9 +38,17 @@ public class App {
             }
             /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
             System.out.println("결과: " + result);
+            Array[count] = result; //연산의 결과를 배열에 저장
+            count++; //index를 증가
+             System.out.println("현재 저장된 배열은 "+count+"개 입니다");
+
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");// 계산식 중단 메세지
             String text = sc.next(); //메세지 입력창 생성
             if(Objects.equals(text, "exit")) { // exit 와 같은지 확인
+                for (int i = 1; i < Array.length; i++) {
+
+                    System.out.println("저장된 배열 " + i + ": " + Array[i]);
+                }
                 break; // 같으면 즉시 중단
             }
             /* exit을 입력 받으면 반복 종료 */
