4c3fb671c305db2ac0121b19cf3a4de754cb561c
Robbie <robbie331@>
2024-07-26 18:53:51 +0900

    1-7 연산결과 무한 저장(List)

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,12 +1,12 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 //
 public class App {
     public static void main(String[] args) {
-        Integer[] array =new Integer[10]; // 연산결과를 10개 저장할 수 있는 배열 생성
-        int count = 0; //연산 결과가 저장된 배열의 마지막 index를 저장하는 변수 선언
+        ArrayList<Integer> list = new ArrayList<>();//연산결과를 무한히 저장할 수 있는 리스트생성
         Scanner sc = new Scanner(System.in);
 
          while(true){
@@ -44,26 +44,27 @@ public class App {
             /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
            if(validOperation) { //validOperation이 true이면(연산오류가 없으면)
                System.out.println("결과: " + result);
-
-               if (count < array.length) { //입력된 값이 배열 크기보다 작으면
-                   array[count] = result; // 배열값에 결과값 저장
-                   count++; //index 증가
-               } else {
-                   System.out.println("배열이 가득 찼습니다. 가장 먼저 저장된 결과를 삭제하고 새결과를 저장합니다.");
-                   System.arraycopy(array, 1, array, 0, array.length - 1);//모든요소를 한 칸 앞으로 이동
-                   array[array.length - 1] = result; //새로운 결과를 마지막에 저장
-               }
-               System.out.println("현재 저장된 배열은 " + count + "개 입니다");
+               list.add(result); //리스트에 결과값 저장
+               System.out.println("현재 저장된 결과는 " + list.size() + "개 입니다");
            }
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");// 계산식 중단 메세지
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료,remove 입력 시 먼저 입력된 결과 삭제)");// 계산식 중단 메세지
             String text = sc.next(); //메세지 입력창 생성
             if(Objects.equals(text, "exit")) { // exit 와 같은지 확인
-                for (int i = 1; i < array.length; i++) {
-                    System.out.println("저장된 배열 " + i + ": " + array[i]);
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println("저장된 결과 " + i + ": " + list.get(i));
                 }
                 break; // 같으면 즉시 중단
             }
-            /* exit을 입력 받으면 반복 종료 */
+            if (Objects.equals(text,"remove")){//remove라고 쓴다면
+                    if (!list.isEmpty()) { //list가 비어있지않으면
+                        list.remove(0);//가장 먼저 저장된 결과 삭제
+                        System.out.println("가정 먼저 저장된 결과를 삭제했습니다");
+                    }else {
+                        System.out.println("저장된 결과가 없습니다.");
+                    }
+            }
+
+
         }
     }
 }
