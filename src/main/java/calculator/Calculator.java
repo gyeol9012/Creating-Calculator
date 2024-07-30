375bc87c998a4d52e6f625f2fe345b2f734321eb
Robbie <robbie331@>
2024-07-30 20:47:56 +0900

    2-1 연산결과 저장하는 컬렉션 생성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,35 @@
 package calculator;
 
-public class Calculator {
+import java.util.ArrayList;
 
-}
+class Calculator {
+    ArrayList<Integer> results = new ArrayList<>();
+    public int calculate(int num1, int num2, char operator) throws ThrowException {
+         int result;
+         switch (operator){
+             case '+':
+                 result = num1+num2;
+                 break;
+             case '-':
+                 result = num1-num2;
+                 break;
+             case '*':
+                 result = num1*num2;
+                 break;
+             case '/':
+                if (num2 == 0) {
+                    throw new ThrowException("분모는 0이 될 수 없습니다.");
+                } else {
+                    result = num1/num2;
+                }
+                break;
+             default:
+                 throw new ThrowException("연산 기호를 확인 후 입력바랍니다.");
+        }
+            results.add(result);
+            return result;
+
+     }
+
+    }
+
---
+++ src/main/java/calculator/ThrowException.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class ThrowException extends Exception {
+    public ThrowException(String message) {
+        super(message);
+    }
+}
